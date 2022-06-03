c5bf470cc78df38ba43115ca517f772db64689a5
Theodore de Lachaux <44366174+tlachaux@users.noreply.github.com>
2022-06-03 12:38:28 +0200

    Add files via upload

--- Virus.py
+++ Virus.py
@@ -1,18 +1,62 @@
-import socket
-import time
-import os
-import subprocess
-
-import pythoncom
-import pywintypes
-import win32api
-from win32com.shell import shell
+import ctypes
+import platform
 
 
 class Virus:
 
+
+
+    """
+    Tries to obtain informations about the hosting system to adapt it's behavior.
+    """
     def __init__(self):
 
-        print(shell.isUserAnAdmin())
+        self.systemInfos    = None
+        self.adminMode      = None
 
+        self.obtainPlatformDescription()
+        self.obtainUserPrivileges()
+
+
+    """
+    Tries to obtain the OS name and version.
+    """
+    def obtainPlatformDescription(self):
+
+        try:
+    
+            self.systemInfos = (platform.system(), platform.release())
+
+        except:
+
+            pass
+
+
+    """
+    Finds out if the user is in admin mode or not.
+    """
+    def obtainUserPrivileges(self):
+
+        try:
+    
+            self.adminMode = (ctypes.windll.shell32.IsUserAnAdmin() == 1)
+
+        except:
+
+            pass
+    
+
+    """
+    Displays informations gathered for debug purposes.
+    """
+    def displayInformations(self):
+
+        print(self.systemInfos)
+        print(self.adminMode)
+
+
+"""
+-----------------------------------------------------
+"""
 virus = Virus()
+virus.displayInformations()
